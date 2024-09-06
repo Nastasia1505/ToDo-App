88c5e1917fcdc2cc9cad2e720439507336d447de
Пользователь <nastya.svirid@mail.ru>
2024-09-06 15:49:26 +0300

    add edit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let input = document.querySelector(".input");
-let addBtn = document.querySelector(".btn");
+let addBtn = document.querySelector(".addBtn");
+let editBtn = document.querySelector(".editBtn");
 let list = document.querySelector(".list");
 
 let Task = function (value) {
@@ -27,10 +28,28 @@ let TaskList = function () {
       task.classList.add("item");
       list.append(task);
 
+      let taskContent = document.createElement("div");
+      taskContent.classList.add("task__content");
+      task.append(taskContent);
+
+      let check = document.createElement("button");
+      if (item.isChecked) {
+        check.classList.add("check");
+      } else {
+      }
+      check.classList.add("action__btn");
+      check.classList.add("action__btn-check");
+      taskContent.append(check);
+
+      let iconCheck = document.createElement("i");
+      iconCheck.classList.add("fa-solid");
+      iconCheck.classList.add("fa-check");
+      check.append(iconCheck);
+
       let taskText = document.createElement("p");
       taskText.classList.add("item__text");
       taskText.textContent = `${item.name}`;
-      task.append(taskText);
+      taskContent.append(taskText);
 
       let action = document.createElement("div");
       action.classList.add("action");
@@ -39,7 +58,7 @@ let TaskList = function () {
       let edit = document.createElement("button");
       edit.classList.add("action__btn");
       edit.classList.add("action__btn-edit");
-      task.append(edit);
+      action.append(edit);
 
       let iconEdit = document.createElement("i");
       iconEdit.classList.add("fa-solid");
@@ -49,33 +68,23 @@ let TaskList = function () {
       let remove = document.createElement("button");
       remove.classList.add("action__btn");
       remove.classList.add("action__btn-trash");
-      task.append(remove);
+      action.append(remove);
 
       let iconTrash = document.createElement("i");
       iconTrash.classList.add("fa-solid");
       iconTrash.classList.add("fa-trash-can");
       remove.append(iconTrash);
 
-      let check = document.createElement("button");
-      if (item.isChecked) {
-        check.classList.add("check");
-      } else {
-      }
-      check.classList.add("action__btn");
-      check.classList.add("action__btn-check");
-      task.append(check);
-
-      let iconCheck = document.createElement("i");
-      iconCheck.classList.add("fa-solid");
-      iconCheck.classList.add("fa-check");
-      check.append(iconCheck);
+      check.addEventListener("click", () => {
+        this.check(item.date);
+      });
 
       remove.addEventListener("click", () => {
         this.remove(item.date);
       });
 
-      check.addEventListener("click", () => {
-        this.check(item.date);
+      edit.addEventListener("click", () => {
+        this.edit(item.date);
       });
 
       input.value = "";
@@ -101,11 +110,28 @@ let TaskList = function () {
     });
     this.render();
   };
+
+  this.edit = function (id) {
+    this.data.forEach((item) => {
+      if (item.date == id) {
+        input.value = `${item.name}`;
+        console.log(input.value);
+        addBtn.classList.add("disable");
+        editBtn.classList.remove("disable");
+        editBtn.addEventListener("click", function () {
+          item.name = input.value;
+          console.log(item);
+          editBtn.classList.add("disable");
+          addBtn.classList.remove("disable");
+          input.value = "";
+        });
+      }
+    });
+  };
 };
 
 let taskList = new TaskList();
 
 addBtn.addEventListener("click", function () {
-  console.log(taskList);
   taskList.add(input.value);
 });
